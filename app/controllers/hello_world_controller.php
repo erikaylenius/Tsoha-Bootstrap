1bd312e4f69a64e04e354080b919526d61fd0298
Erika Ylenius <eylenius@dhcp-asv-116.helsinki.fi>
2017-05-29 21:44:12 +0300

    html-sivuja

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,46 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  // View::make('home.html');
+      echo 'Tämä on etusivu';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      // echo 'Hello World!';
+      View::make('helloworld.html');
     }
+
+    public static function etusivu(){
+      View::make('suunnitelmat/etusivu.html');
+    }
+
+    // ylläpidon näkymät
+
+    public static function asiakkaat(){
+      View::make('suunnitelmat/asiakkaat.html');
+    }
+
+    public static function asiakas(){
+      View::make('suunnitelmat/asiakas.html');
+    }
+
+    public static function tilaukset(){
+      View::make('suunnitelmat/tilaukset.html');
+    }    
+
+    // asiakkaan näkymät
+
+    public static function rekisteroidy(){
+      View::make('suunnitelmat/rekisteroidy.html');
+    }
+
+    public static function omattiedot(){
+      View::make('suunnitelmat/omattiedot.html');
+    }
+
+    public static function uusitilaus(){
+      View::make('suunnitelmat/uusitilaus.html');
+    }
+
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,31 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/asiakkaat', function() {
+    HelloWorldController::asiakkaat();
+  });
+
+  $routes->get('/asiakas', function() {
+    HelloWorldController::asiakas();
+  });
+
+  $routes->get('/etusivu', function() {
+    HelloWorldController::etusivu();
+  });
+
+  $routes->get('/omattiedot', function() {
+    HelloWorldController::omattiedot();
+  }); 
+
+  $routes->get('/rekisteroidy', function() {
+    HelloWorldController::rekisteroidy();
+  }); 
+
+  $routes->get('/tilaukset', function() {
+    HelloWorldController::tilaukset();
+  });
+
+  $routes->get('/uusitilaus', function() {
+    HelloWorldController::uusitilaus();
+  });
